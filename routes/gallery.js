bd654bd1df6132faca8d2e04b298df82f1b579cc
allcomputersarebad <allcomputersarebad@users.noreply.github.com>
2023-01-27 21:16:14 -0700

    accept photo slug in gallery routes

--- routes/gallery.js
+++ routes/gallery.js
@@ -5,44 +5,52 @@ const galleryRouter = express.Router();
 
 galleryRouter.use(express.static("gallery"));
 
+// TODO: restrict outgoing fields
 galleryRouter.param("slug", async function (req, res, next, slugParam) {
   console.log("gallery param slug", slugParam);
   req.gallery = (
     await db.Gallery.findOne({
       where: { slug: slugParam },
-      include: [{ model: db.Photo }],
+      include: db.Photo,
     })
   )?.dataValues;
-  console.log("gallery param slug retrieved", req.gallery);
+  next();
+});
+
+galleryRouter.param("photo", async function (req, res, next, photoSlug) {
+  console.log("gallery param photoSlug", photoSlug);
+  req.photo = (
+    await db.Photo.findOne({
+      where: { slug: photoSlug },
+    })
+  )?.dataValues;
   next();
 });
 
 galleryRouter.get("/:slug", function (req, res, next) {
-  if (!req.gallery) {
+  if (!req.gallery || req.gallery?.hidden) {
     // res.sendStatus(404);
     res.status(404);
     next();
   } else {
-    console.log("gallery get", req.gallery);
     if (req.accepts("html")) {
       res.render("index", {
         title: "Gallery " + req.gallery.name,
         gallery: req.gallery,
       });
     } else if (req.accepts("json")) {
-      // TODO: restrict fields
+      // TODO: restrict outgoing fields
       res.json(req.gallery);
     }
   }
 });
 
-/* // static route
+// static route
 galleryRouter.get("/:slug/:photo", function (req, res, next) {
-    res.render("photo", {
-      gallery: req.gallery,
-      photo: req.photo,
-    });
+  res.render("photo", {
+    gallery: req.gallery,
+    photo: req.photo,
+  });
 });
-*/
 
 export default galleryRouter;
