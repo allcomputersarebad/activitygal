39dcc55366accf8e39356409031679a63a63052d
Annie <annie.gallagher7@gmail.com>
2023-02-06 12:31:32 -0700

    fixed loop to use actual database data

--- routes/galleries.js
+++ routes/galleries.js
@@ -5,41 +5,13 @@ const galleriesRouter = express.Router();
 
 /* GET galleries page. */
 galleriesRouter.get("/", async function (req, res, next) {
-  const allGalleries = await db.Gallery.findAll({
-    include: [db.title, db.slug],
-  });
+  console.log("entering galleries route");
+  const allGalleries = await db.Gallery.findAll({});
+  console.log(allGalleries.map((g) => g.dataValues));
   res.render("galleries", {
     title: "Galleries",
-    galleries: allGalleries,
+    galleries: allGalleries.map((g) => g.dataValues),
   });
 });
 
 export default galleriesRouter;
-// galleryRouter.get("/:slug", function (req, res, next) {
-//   console.log("rendering gallery", req.gallery);
-//   if (!req.gallery || req.gallery?.hidden) {
-//     // res.sendStatus(404);
-//     res.status(404);
-//     next();
-//   } else {
-//     if (req.accepts("html")) {
-//       res.render("gallery", {
-//         title: req.gallery.title,
-//         gallery: req.gallery,
-//         photos: req.gallery?.Photos.map(
-//           ({ altText, caption, description, path, title, uuid }) => ({
-//             altText,
-//             caption,
-//             description,
-//             path,
-//             title,
-//             uuid,
-//           })
-//         ),
-//       });
-//     } else if (req.accepts("json")) {
-//       // TODO: restrict outgoing fields
-//       res.json(req.gallery);
-//     }
-//   }
-// });
