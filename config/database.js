1524a208fc37c64e24f0ef680a05b7f24b1b9d1b
allcomputersarebad <allcomputersarebad@users.noreply.github.com>
2023-02-08 22:39:58 -0800

    db config nonsense

--- config/database.js
+++ config/database.js
@@ -4,19 +4,21 @@ import path from "path";
 const database = {
   production: {
     dialect: "postgres",
-    uri: process.env.DB_URI,
+    uri: process.env.DB_URI ?? "",
   },
   test: {
     dialect: "sqlite",
-    storage: ":memory:",
+    storage:
+      process.env.PERSISTENT_STORAGE && process.env.DB_TEST
+        ? path.resolve(process.env.PERSISTENT_STORAGE, process.env.DB_TEST)
+        : ":memory:",
   },
   development: {
     dialect: "sqlite",
-    storage: path.resolve(
-      // undefined ?? "" to prevent err
-      process.env.PERSISTENT_STORAGE ?? "",
-      process.env.DB_FILE ?? "dev.sqlite"
-    ),
+    storage:
+      process.env.PERSISTENT_STORAGE && process.env.DB_FILE
+        ? path.resolve(process.env.PERSISTENT_STORAGE, process.env.DB_FILE)
+        : undefined,
   },
 }[process.env.NODE_ENV];
 
