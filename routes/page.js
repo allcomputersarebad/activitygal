1e9197350ad0fae9bde2f4c415d39a331b1903f4
allcomputersarebad <allcomputersarebad@users.noreply.github.com>
2023-02-09 13:26:52 -0800

    fix index

--- routes/page.js
+++ routes/page.js
@@ -22,12 +22,12 @@ pageRouter.get("/", function (req, res, next) {
 
 /* GET home page. */
 pageRouter.get("/", async function (req, res, next) {
-  page = await db.Page.findOne({
+  const page = await db.Page.findOne({
     where: { slug: "index" },
     include: [db.Gallery],
   });
   res.render("welcome", {
-    ...page,
+    ...(page ?? {}),
     title: page?.title ?? "ActivityGal",
   });
 });
@@ -37,7 +37,6 @@ pageRouter.get("/:slug", function (req, res, next) {
     res.render("index", {
       title: req.page.title,
       //galleries: page?.Galleries,
-      //photos: page?.Photos,
     });
   } else {
     //res.status(404); // TODO: wtf
